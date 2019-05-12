cebff8008b9ac772c2f006520f763373838461a4
Tasso <43638620+26689@users.noreply.github.com>
2019-05-12 17:39:15 +0200

    Update dbVars.php
    database veranderd omdat de local database ajax3 niet werkt op github

--- AJAX3/dbVars.php
+++ AJAX3/dbVars.php
@@ -2,12 +2,12 @@
 //$servername = "127.0.0.1";
 //$uid = "tasso2001";
 //$pwd = "1234567890";
-//$database = "ajax3";
+//$database = "c3637test2";
 // weet niet waarom dit niet werkt want wel aangemaakt op de server
 
 
 $servername = "127.0.0.1";
 $uid = "root";
 $pwd = "";
-$database = "ajax3";
+$database = "c3637test2";
 ?>
