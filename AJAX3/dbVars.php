b7881cb8c6aa60e2fccd67ba8cb809ec52202011
Tasso <43638620+26689@users.noreply.github.com>
2019-05-12 17:27:01 +0200

    Add files via upload

---
+++ AJAX3/dbVars.php
@@ -0,0 +1,13 @@
+<?php
+//$servername = "127.0.0.1";
+//$uid = "tasso2001";
+//$pwd = "1234567890";
+//$database = "ajax3";
+// weet niet waarom dit niet werkt want wel aangemaakt op de server
+
+
+$servername = "127.0.0.1";
+$uid = "root";
+$pwd = "";
+$database = "ajax3";
+?>
